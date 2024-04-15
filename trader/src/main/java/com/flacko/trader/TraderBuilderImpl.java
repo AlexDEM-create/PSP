4dc5c547e770a4ce59a93cf428f8e4e5b8165313
alexd <1090424@gmail.com>
2024-04-16 01:03:00 +0300

    Corrected Traders 80%

--- trader/src/main/java/com/flacko/trader/TraderBuilderImpl.java
+++ trader/src/main/java/com/flacko/trader/TraderBuilderImpl.java
@@ -6,10 +6,18 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.util.Optional;
+
 @Component
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
 @RequiredArgsConstructor
-public class TraderBuilderImpl   implements TraderBuilder {
+public class TraderBuilderImpl   implements InitializableTraderBuilder {
+
+    private final Instant now = Instant.now();
+
+    private TraderPojo.TraderPojoBuilder pojoBuilder;
+
     @Override
     public TraderBuilder withId(String id) {
         return null;
@@ -21,21 +29,107 @@ public class TraderBuilderImpl   implements TraderBuilder {
     }
 
     @Override
+    public TraderBuilder withUserId(String name) {
+        return null;
+    }
+
+    @Override
+    public TraderBuilder withArchived() {
+        return null;
+    }
+
+    @Override
     public Trader build() throws TraderMissingRequiredAttributeException {
         return null;
     }
 
+    @Override
+    public TraderBuilder initializeNew() {
+        pojoBuilder = TraderPojo.builder()
+                .createdDate(now)
+                .updatedDate(now);
+        return this;
+    }
+
+    @Override
+    public TraderBuilder initializeExisting(Trader existingTrader) {
+        this.id = existingTrader.getId();
+        this.name = existingTrader.getName();
+        this.userId = existingTrader.getUserId();
+        this.tradersTeam = existingTrader.getTradersTeam();
+        return this;
+    }
+}
+
+
+
+
+//    private final Instant now = Instant.now();
+//
+//    private MerchantPojo.MerchantPojoBuilder pojoBuilder;
+//
+
+
 //    @Override
-//    public TraderBuilder initializeNew() {
+    public MerchantBuilder initializeNew() {
+        pojoBuilder = MerchantPojo.builder()
+                .createdDate(now)
+                .updatedDate(now);
+        return this;
+    }
+//
+//
+//    @Override
+//    public MerchantBuilder initializeExisting(Merchant existingMerchant) {
+//        pojoBuilder = MerchantPojo.builder()
+//                .id(existingMerchant.getId())
+//                .name(existingMerchant.getName())
+//                .createdDate(existingMerchant.getCreatedDate())
+//                .updatedDate(now);
+//        pojoBuilder.userId(existingMerchant.getUserId().orElse(null));
 //        return this;
 //    }
 //
 //    @Override
-//    public TraderBuilder initializeExisting(Trader existingTrader) {
-//        this.id = existingTrader.getId();
-//        this.name = existingTrader.getName();
-//        this.userId = existingTrader.getUserId();
-//        this.tradersTeam = existingTrader.getTradersTeam();
+//    public MerchantBuilder withId(String id) {
+//        pojoBuilder.id(id);
 //        return this;
 //    }
-}
+//
+//    @Override
+//    public MerchantBuilder withName(String name) {
+//        pojoBuilder.name(name);
+//        return this;
+//    }
+//
+//    @Override
+//    public MerchantBuilder withUserId(String userId) {
+//        pojoBuilder.userId(userId);
+//        return this;
+//    }
+//
+//    @Override
+//    public MerchantBuilder withArchived() {
+//        pojoBuilder.deletedDate(now);
+//        return this;
+//    }
+//
+//    @Override
+//    public Merchant build() throws MerchantMissingRequiredAttributeException {
+//        MerchantPojo merchant = pojoBuilder.build();
+//        validate(merchant);
+//        return merchant;
+//    }
+//
+//    private void validate(MerchantPojo merchant) throws MerchantMissingRequiredAttributeException {
+//        if (merchant.getId() == null || merchant.getId().isEmpty()) {
+//            throw new MerchantMissingRequiredAttributeException("id", Optional.empty());
+//        }
+//        if (merchant.getName() == null || merchant.getName().isEmpty()) {
+//            throw new MerchantMissingRequiredAttributeException("name", Optional.of(merchant.getId()));
+//        }
+//        if (merchant.getUserId() == null || merchant.getUserId().isEmpty()) {
+//            throw new MerchantMissingRequiredAttributeException("userId", Optional.of(merchant.getId()));
+//        }
+//    }
+//}
--- trader/src/main/java/com/flacko/trader/rest/TraderResponse.java
+++ trader/src/main/java/com/flacko/trader/rest/TraderResponse.java
@@ -6,8 +6,8 @@ import static javax.swing.Action.NAME;
 import static org.springframework.data.jpa.domain.AbstractPersistable_.ID;
 
 public record TraderResponse(@JsonProperty(ID) String id,
-                             @JsonProperty(NAME) String name)
-                             @JsonProperty(USER_ID) String userId,{
+                             @JsonProperty(NAME) String name,
+                             @JsonProperty(USER_ID) String userId) {
     private static final String ID = "id";
     private static final String NAME = "name";
     private static final String USER_ID = "userId";
