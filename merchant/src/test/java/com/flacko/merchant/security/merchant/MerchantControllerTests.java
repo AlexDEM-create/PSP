da1d603f4ebff9aee5209f9ee8ba606df1809bc2
alexd <1090424@gmail.com>
2024-04-26 01:24:15 +0300

    MerchantControllerTest has been rewritten

--- merchant/src/test/java/com/flacko/merchant/security/merchant/MerchantControllerTests.java
+++ merchant/src/test/java/com/flacko/merchant/security/merchant/MerchantControllerTests.java
@@ -1,35 +1,20 @@
 package com.flacko.merchant.security.merchant;
 
-import com.flacko.auth.spring.ServiceLocator;
-import com.flacko.merchant.MerchantRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flacko.merchant.MerchantService;
+import com.flacko.merchant.rest.MerchantCreateRequest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import java.math.BigDecimal;
-
-import static org.hamcrest.Matchers.*;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 public class MerchantControllerTests {
 
     @Autowired
@@ -38,48 +23,66 @@ public class MerchantControllerTests {
     @Autowired
     private MerchantService merchantService;
 
-    @Autowired
-    private MerchantRepository merchantRepository;
+    private String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     public void testListMerchants() throws Exception {
         merchantService.create()
                 .withName("testMerchant1")
                 .withUserId("testUser1")
-                .withIncomingFeeRate(BigDecimal.valueOf(0.05))
-                .withOutgoingFeeRate(BigDecimal.valueOf(0.05))
+                .withIncomingFeeRate(0.05)
+                .withOutgoingFeeRate(0.05)
                 .build();
         merchantService.create()
                 .withName("testMerchant2")
                 .withUserId("testUser2")
-                .withIncomingFeeRate(BigDecimal.valueOf(0.05))
-                .withOutgoingFeeRate(BigDecimal.valueOf(0.05))
+                .withIncomingFeeRate(0.05)
+                .withOutgoingFeeRate(0.05)
                 .build();
 
-        mockMvc.perform(get("/merchants"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].id", notNullValue()))
-                .andExpect(jsonPath("$[0].name", is("testMerchant1")))
-                .andExpect(jsonPath("$[0].userId", is("testUser1")))
-                .andExpect(jsonPath("$[0].incomingFeeRate", is(0.05)))
-                .andExpect(jsonPath("$[0].outgoingFeeRate", is(0.05)))
-                .andExpect(jsonPath("$[1].id", notNullValue()))
-                .andExpect(jsonPath("$[1].name", is("testMerchant2")))
-                .andExpect(jsonPath("$[1].userId", is("testUser2")))
-                .andExpect(jsonPath("$[1].incomingFeeRate", is(0.05)))
-                .andExpect(jsonPath("$[1].outgoingFeeRate", is(0.05)));
+        this.mockMvc.perform(get("/merchants"))
+                .andExpect(status().isOk());
     }
 
-    @TestConfiguration
-    static class TestConfig {
-        @Bean
-        public MockMvc mockMvc(WebApplicationContext webApplicationContext) {
-            return MockMvcBuilders
-                    .webAppContextSetup(webApplicationContext)
-                    .apply(springSecurity())
-                    .build();
-        }
+    @Test
+    public void testGetMerchant() throws Exception {
+        this.mockMvc.perform(get("/merchants/{merchantId}"))
+                .andExpect(status().isOk());
     }
 
+    @Test
+    public void testCreateMerchant() throws Exception {
+        MerchantCreateRequest request = new MerchantCreateRequest("name", "userId", BigDecimal.TEN, BigDecimal.TEN);
+        this.mockMvc.perform(post("/merchants").content(asJsonString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testArchiveMerchant() throws Exception {
+        this.mockMvc.perform(delete("/merchants/{merchantId}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testNotFoundException() throws Exception {
+        this.mockMvc.perform(get("/merchants/{invalidId}"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testBadRequestException() throws Exception {
+        MerchantCreateRequest request = new MerchantCreateRequest("", "userId", BigDecimal.TEN, BigDecimal.TEN);
+        this.mockMvc.perform(post("/merchants").content(asJsonString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
 }
