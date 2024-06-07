fd1ce8fd5af88d6f818d1ee1d62e1a54f031c265
angelbeatzprod <angelbeatzprod@gmail.com>
2024-06-07 23:33:09 +0300

    One more attempts to fix python invisible modules

--- receipt-data-extractor/webapp/receipt_payment_verification.py
+++ receipt-data-extractor/webapp/receipt_payment_verification.py
@@ -1,8 +1,5 @@
-import pip
-installed_packages = pip.get_installed_distributions()
-installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
-                                  for i in installed_packages])
-print(installed_packages_list)
+import sys
+sys.path.append('/usr/local/lib/python3.12/site-packages')
 
 import PyPDF2
 import logging
