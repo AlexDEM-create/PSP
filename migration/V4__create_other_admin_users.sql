a728de47d2684427923cb17803c79c993ae2fa50
angelbeatzprod <angelbeatzprod@gmail.com>
2024-06-11 00:39:38 +0300

    Added more admin users

---
+++ migration/V4__create_other_admin_users.sql
@@ -0,0 +1,2 @@
+insert into users (banned, created_date, updated_date, id, login, password, role) values (false, '2024-06-09 23:59:53.036239+03', '2024-06-09 23:59:53.036239+03', 'sevvos6r7hni1vqiqytj', 'darth_vader', '$2a$10$Uww3soCEZsjHHdVQKBNPceLDu0oCnVHf3e9yrZDgZfiuoeHNTWDly', 'USER_ADMIN');
+insert into users (banned, created_date, updated_date, id, login, password, role) values (false, '2024-06-09 23:59:53.036239+03', '2024-06-09 23:59:53.036239+03', 'sevvvg3etwt4z1ivlhm1', 'rodi', ' $2a$10$WPjeL8wYqEhv0ONbF6AY0OLH2MyQeUCjouxUP1nHG8.iqjSgOLrhC', 'USER_ADMIN');
