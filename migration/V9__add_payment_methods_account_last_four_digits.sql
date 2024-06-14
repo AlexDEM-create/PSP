cf44faad4a06d227fa7f7e0c7ab2a3454a2774b7
angelbeatzprod <angelbeatzprod@gmail.com>
2024-06-15 01:39:20 +0300

    Account last four digts and bug fixes

--- migration/V9__add_payment_methods_account_last_four_digits.sql
+++ migration/V9__add_payment_methods_account_last_four_digits.sql
@@ -1,2 +1,4 @@
 ALTER TABLE payment_methods
     ADD COLUMN account_last_four_digits VARCHAR(4) NOT NULL DEFAULT '0000';
+ALTER TABLE payment_methods
+    ALTER COLUMN account_last_four_digits DROP DEFAULT;
